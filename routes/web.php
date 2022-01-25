d08bb2fccdb2069ffa7a2deda55525c77f8d2dd1
faridnubaili <Faridnubaili@gmail.com>
2022-01-25 11:28:23 +0700

    selesai bagian model

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,65 +30,19 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Farid Nubaili",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum suscipit ipsum nihil voluptatem dolores animi, pariatur officiis odit excepturi laboriosam rem quasi eum maxime amet aut accusantium magni? Iusto debitis saepe omnis maiores, itaque officiis! Ea voluptates iure dolorum tempore iusto harum deserunt quis perspiciatis itaque, unde, dolore est laboriosam. Quam praesentium possimus, consectetur numquam quaerat ad quidem amet, reprehenderit deserunt qui, mollitia fuga. Reiciendis officia iste impedit suscipit deleniti rerum nam nemo! Est fugiat rerum minus unde debitis tempora."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Lionel Messi",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum suscipit ipsum nihil voluptatem dolores animi, pariatur officiis odit excepturi laboriosam rem quasi eum maxime amet aut accusantium magni? Iusto debitis saepe omnis maiores, itaque officiis! Ea voluptates iure dolorum tempore iusto harum deserunt quis perspiciatis itaque, unde, dolore est laboriosam. Quam praesentium possimus, consectetur numquam quaerat ad quidem amet, reprehenderit deserunt qui, mollitia fuga. Reiciendis officia iste impedit suscipit deleniti rerum nam nemo! Est fugiat rerum minus unde debitis tempora."
-        ],
-        [
-            "title" => "Judul Post Ketiga",
-            "slug" => "judul-post-ketiga",
-            "author" => "Cristiano Ronaldo",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum suscipit ipsum nihil voluptatem dolores animi, pariatur officiis odit excepturi laboriosam rem quasi eum maxime amet aut accusantium magni? Iusto debitis saepe omnis maiores, itaque officiis! Ea voluptates iure dolorum tempore iusto harum deserunt quis perspiciatis itaque, unde, dolore est laboriosam. Quam praesentium possimus, consectetur numquam quaerat ad quidem amet, reprehenderit deserunt qui, mollitia fuga. Reiciendis officia iste impedit suscipit deleniti rerum nam nemo! Est fugiat rerum minus unde debitis tempora."
-        ]
-    ];
+
 
     return view('posts', [
         "title" => "Posts",
-        "posts" => $blog_posts
+        "posts" => Post::all()
     ]);
 });
 
 //Route Single Post
 Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Farid Nubaili",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum suscipit ipsum nihil voluptatem dolores animi, pariatur officiis odit excepturi laboriosam rem quasi eum maxime amet aut accusantium magni? Iusto debitis saepe omnis maiores, itaque officiis! Ea voluptates iure dolorum tempore iusto harum deserunt quis perspiciatis itaque, unde, dolore est laboriosam. Quam praesentium possimus, consectetur numquam quaerat ad quidem amet, reprehenderit deserunt qui, mollitia fuga. Reiciendis officia iste impedit suscipit deleniti rerum nam nemo! Est fugiat rerum minus unde debitis tempora."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Lionel Messi",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum suscipit ipsum nihil voluptatem dolores animi, pariatur officiis odit excepturi laboriosam rem quasi eum maxime amet aut accusantium magni? Iusto debitis saepe omnis maiores, itaque officiis! Ea voluptates iure dolorum tempore iusto harum deserunt quis perspiciatis itaque, unde, dolore est laboriosam. Quam praesentium possimus, consectetur numquam quaerat ad quidem amet, reprehenderit deserunt qui, mollitia fuga. Reiciendis officia iste impedit suscipit deleniti rerum nam nemo! Est fugiat rerum minus unde debitis tempora."
-        ],
-        [
-            "title" => "Judul Post Ketiga",
-            "slug" => "judul-post-ketiga",
-            "author" => "Cristiano Ronaldo",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum suscipit ipsum nihil voluptatem dolores animi, pariatur officiis odit excepturi laboriosam rem quasi eum maxime amet aut accusantium magni? Iusto debitis saepe omnis maiores, itaque officiis! Ea voluptates iure dolorum tempore iusto harum deserunt quis perspiciatis itaque, unde, dolore est laboriosam. Quam praesentium possimus, consectetur numquam quaerat ad quidem amet, reprehenderit deserunt qui, mollitia fuga. Reiciendis officia iste impedit suscipit deleniti rerum nam nemo! Est fugiat rerum minus unde debitis tempora."
-        ]
-    ];
-
-    $new_post = [];
-    foreach ($blog_posts as $post) {
-        if ($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
 
     return view('post', [
         "title" => "SinglePost",
-        "post" => $new_post
+        "post" => Post::find($slug)
     ]);
 });
